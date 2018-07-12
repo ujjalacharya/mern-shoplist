5ce403120a56af8950e9c6f0332405eb92e2efb4
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-12 20:53:54 +0545

    client setup and reactstrap

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark light expand="md" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">MERN Shop</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
