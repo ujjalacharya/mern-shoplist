658e413cd5d15b41d3ca9f4829dd3ccdc323a979
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-14 01:49:43 +0545

    final touch for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
+const path = require('path')
 
 
 //Import database file
@@ -12,9 +13,9 @@ const db = require('./config/db')
 const items = require('./controller/routes/api/items');
 
 //Connect to the database
-mongoose.connect(db.mongoURI, {useNewUrlParser: true})
-        .catch(err =>  console.log(err))
-        .then(()=>console.log('Successfully connected to the database'))
+mongoose.connect(db.mongoURI, { useNewUrlParser: true })
+    .catch(err => console.log(err))
+    .then(() => console.log('Successfully connected to the database'))
 
 // BodyParser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -23,7 +24,15 @@ app.use(bodyParser.json())
 //Handle routes
 app.use('/api/items', items);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 //Server run
-app.listen(PORT , ()=>{
+app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`)
 })
